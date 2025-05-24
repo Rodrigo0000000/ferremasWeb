8ba1463eae221df7b8a4fb7ce1819b297fcc860e
Rodrigo0000000 <rodri.hernadezc@duocuc.cl>
2025-05-24 08:37:05 -0400

    Pequeños cambios

--- static/js/dropdowns.js
+++ static/js/dropdowns.js
@@ -1,21 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const dropdowns = document.querySelectorAll('.dropdown-btn');
-
-  dropdowns.forEach(btn => {
-    btn.addEventListener('click', () => {
-      btn.classList.toggle('active');
-      const container = btn.nextElementSibling;
-
-      if (container.style.maxHeight) {
-        container.style.maxHeight = null;
-      } else {
-        container.style.maxHeight = container.scrollHeight + "px";
-      }
-    });
-  });
-});
-
-
 function initSidebarDropdowns() {
 const sidebarButtons = document.querySelectorAll('.sidebar .dropdown-btn');
 sidebarButtons.forEach((btn) => {
